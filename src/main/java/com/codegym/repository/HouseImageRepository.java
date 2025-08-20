ee519af78818271c781a57d5a2667877b892b9eb
MarcoHoang <hoang111uuu@gmail.com>
2025-08-20 10:38:14 +0700

    update profile house

--- src/main/java/com/codegym/repository/HouseImageRepository.java
+++ src/main/java/com/codegym/repository/HouseImageRepository.java
@@ -3,6 +3,8 @@ package com.codegym.repository;
 import com.codegym.entity.House;
 import com.codegym.entity.HouseImage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -12,5 +14,6 @@ public interface HouseImageRepository extends JpaRepository<HouseImage, Long> {
 
     List<HouseImage> findByHouseOrderBySortOrderAsc(House house);
 
-    Integer findMaxSortOrderByHouse(House house);
+    @Query("SELECT MAX(hi.sortOrder) FROM HouseImage hi WHERE hi.house = :house")
+    Integer findMaxSortOrderByHouse(@Param("house") House house);
 }
